74b8ef523e1ca5e682b1a297ea465047661628b6
christian <christianbiringanine22@gmail.com>
2022-08-25 16:35:12 +0200

    Add validations on the model User and Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,19 @@
 class Post < ApplicationRecord
+  # Associations
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
+
+  # Validations
+
+  validates_presence_of :title, :text
+
+  # You could use tokenizer option of the length validator, to count only the words, not the line breaks.
+  validates :title, length: {
+    maximum: 250,
+    tokenizer: lambda { |str| str.scan(/\w+/) }
+  }
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_posts_counter
     author.update(posts_counter: author.posts.count)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+  # Associations
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
+
+  # Validation
+
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def three_last_posts
     posts.order('created_at').last(3)
