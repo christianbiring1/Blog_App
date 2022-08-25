09b64077c802bebeb11e7539aabb158bbdba3331
christian <christianbiringanine22@gmail.com>
2022-08-25 13:40:51 +0200

    Fix linter issues

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
-
   def update_comments_counter
     post.update(comments_counter: post.comments.count)
   end
