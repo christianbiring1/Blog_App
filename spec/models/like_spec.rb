31d82123657ef3e9836cfada02e068eef5fd4b08
christian <christianbiringanine22@gmail.com>
2022-08-26 15:41:57 +0200

    Add the associations and custom method test for the test model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,4 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   #tests go here
+  before :each do
+    @author = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    @post = Post.create(title: 'title', text: 'Some text', author_id: @author.id)
+    @like = Like.create(author_id: @author.id, post_id: @post.id)
+  end
+
+  it 'Should be valid' do
+    expect(@like).to be_valid
+  end
+
+  context 'Associations' do
+
+    it 'Should belong to an Author' do
+      assc = described_class.reflect_on_association(:author)
+      expect(assc.macro).to eq :belongs_to
+    end
+
+    it 'Should belong to a post' do
+      assc = described_class.reflect_on_association(:post)
+      expect(assc.macro).to eq :belongs_to
+    end
+  end
+
+  context 'custom methods' do
+    
+    it 'Should update the likes' do
+      expect(@like.update_like_counter).to eq(@post.update(likes_counter: @post.likes.count))
+    end
+  end
 end
