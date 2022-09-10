a9ea82f66726b2ab5884e804cc179d1efff4f0ff
christian <christianbiringanine22@gmail.com>
2022-09-10 23:52:29 +0200

    Add the api

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable,
+         :jwt_authenticatable,
+         jwt_revocation_strategy: JwtDenylist
   # Associations
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
