d15d58385d369b65ae1f2eb0ee1544658304599e
christian <christianbiringanine22@gmail.com>
2022-09-10 10:02:05 +0200

    Remove unnecessaries comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
   def create
-    # @post = Post.find(params[:post_id])
     @comment = current_user.comments.new(comment_params)
     @comment.post_id = params[:post_id]
 
