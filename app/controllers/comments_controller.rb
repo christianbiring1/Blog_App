6e536b079d0e02c1b36df5c31010a2aa5745bdd4
christian <christianbiringanine22@gmail.com>
2022-09-10 18:32:03 +0200

    Add the delete commnets and posts functionalities

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,28 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
+  before_action :authenticate_user!
+
   def create
+    @user = current_user
     @comment = current_user.comments.new(comment_params)
     @comment.post_id = params[:post_id]
 
     if @comment.save
       redirect_to user_post_comments_path(current_user, @comment.post_id)
-      flash[:success] = 'Comment created successfully'
+      flash[:notice] = 'Comment created successfully'
     else
       render :create
+      flash[:alert] = 'Comment wasn\'t created'
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      flash[:notice] = 'comment deleted successfully!'
+      redirect_to user_post_path(current_user, @comment.post_id)
+    else
+      flash[:alert] = 'comment wasn\'t deleted'
     end
   end
 
